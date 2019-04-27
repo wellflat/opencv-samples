14e07ac0beff020db8371e672eca47c1fc3d747f
wellflat <wellflat@gmail.com>
2019-04-28 00:15:46 +0900

    added python sample

--- quality/quality_sample.cpp
+++ quality/quality_sample.cpp
@@ -5,12 +5,16 @@
 int main(int argc, char **argv) {
     using namespace std;
 
-    const string file_name = "lenna.jpg";
+    if(argc != 2) {
+        cerr << "2 arguments required ./quality_sample <image path>" << endl;
+        exit(1);
+    }
+    
     const string model_path = "./brisque_model_live.yml";
     const string range_path = "./brisque_range_live.yml";
-    const cv::Mat input_image = cv::imread(file_name, cv::IMREAD_COLOR);
+    const cv::Mat input_image = cv::imread(argv[1], cv::IMREAD_COLOR);
     shared_ptr<cv::quality::QualityBase> quality(cv::quality::QualityBRISQUE::create(model_path, range_path));
-    cv::Scalar result = quality->compute(input_image);
-    cout << result << endl;
+    cv::Scalar score = quality->compute(input_image);
+    cout << score[0] << endl;
     return 0;
 }
---
+++ quality/quality_sample.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cv2
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('2 arguments required ./quality_sample.py <image path>')
+        sys.exit(-1)
+
+    model_path = "./brisque_model_live.yml"
+    range_path = "./brisque_range_live.yml"
+    input_image = cv2.imread(sys.argv[1])
+    quality = cv2.quality.QualityBRISQUE_create(model_path, range_path)
+    score = quality.compute([input_image])
+    print(score[0])
